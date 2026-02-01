3c231873ddb6148a3842740425941c31efbe0376
Harshit khanna <harshitkhanna19@gmail.com>
2026-02-02 01:02:58 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use std::io::{BufRead, Write, stdout};
 
 fn main() {
-    print!("$ ");
-    stdout().flush().unwrap();
     let stdin = std::io::stdin();
     let mut stdin_handle = stdin.lock();
     let mut stdin_buffer = String::new();
-    match stdin_handle.read_line(&mut stdin_buffer) {
-        Ok(_) => {
-            println!("{}: command not found", stdin_buffer.trim());
+    loop {
+        print!("$ ");
+        let flush_stdout = stdout().flush();
+        if let Err(e) = flush_stdout {
+            eprintln!("Error flushing stdout: {}", e);
+            return;
         }
-        Err(e) => {
-            eprintln!("Error reading input: {}", e);
+        match stdin_handle.read_line(&mut stdin_buffer) {
+            Ok(0) => break,
+            Ok(_) => {
+                let trimmed_buffer = stdin_buffer.trim();
+                if trimmed_buffer.is_empty() {
+                    stdin_buffer.clear();
+                    continue;
+                }
+                println!("{}: command not found", stdin_buffer.trim());
+                stdin_buffer.clear();
+            }
+            Err(e) => {
+                eprintln!("Error reading input: {}", e);
+            }
         }
     }
 }
