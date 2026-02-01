e373bdf64b7850b58ac1611877d3685ea700f894
Harshit khanna <harshitkhanna19@gmail.com>
2026-02-02 00:25:09 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
-
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ")
 }
