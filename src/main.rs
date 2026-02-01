4590510df0f3768eda239cd3839103326427304f
Harshit khanna <harshitkhanna19@gmail.com>
2026-02-02 00:42:22 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{BufRead, Write, stdout};
+
 fn main() {
-    print!("$ ")
+    print!("$ ");
+    stdout().flush().unwrap();
+    let stdin = std::io::stdin();
+    let mut stdin_handle = stdin.lock();
+    let mut stdin_buffer = String::new();
+    match stdin_handle.read_line(&mut stdin_buffer) {
+        Ok(_) => {
+            println!("{}: command not found", stdin_buffer.trim());
+        }
+        Err(e) => {
+            eprintln!("Error reading input: {}", e);
+        }
+    }
 }
