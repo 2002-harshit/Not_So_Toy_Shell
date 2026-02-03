c1afce131d97f1f88c484c5ae1f0bd8bc988ed17
Harshit khanna <harshitkhanna19@gmail.com>
2026-02-03 23:39:35 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use std::{
     env,
-    io::{BufRead, Write, stdout},
-    os::unix::fs::PermissionsExt,
+    ffi::CString,
+    io::{BufRead, Write, stdin, stdout},
+    os::{
+        fd::AsFd,
+        unix::{ffi::OsStrExt, fs::PermissionsExt},
+    },
     path::PathBuf,
+    process::exit,
 };
 
 use nix::{
-    sys::wait::{WaitPidFlag, waitpid},
-    unistd::{ForkResult, fork},
+    sys::{
+        signal::{SaFlags, SigAction, SigSet, Signal, sigaction},
+        wait::waitpid,
+    },
+    unistd::{ForkResult, Pid, execv, fork, getpgrp, getpid, setpgid, tcsetpgrp},
 };
 
 struct Commands;
@@ -62,10 +70,59 @@ fn handle_non_builtins(command: &str, args: &[&str]) {
     if let Some(full_path) = is_command_in_paths_env(command) {
         match unsafe { fork() } {
             Ok(ForkResult::Child) => {
-                println!("Mf call a exec family call here");
+                /* resetting the signals */
+                let sa = SigAction::new(
+                    nix::sys::signal::SigHandler::SigDfl,
+                    SaFlags::empty(),
+                    SigSet::empty(),
+                );
+                unsafe {
+                    let _ = sigaction(Signal::SIGTTOU, &sa);
+                    let _ = sigaction(Signal::SIGTTIN, &sa);
+                }
+                let pid = getpid().as_raw();
+                if let Err(e) = setpgid(Pid::from_raw(0), Pid::from_raw(0)) {
+                    eprintln!(
+                        "Pid: {}, setpgid failed: {}(Code: {})",
+                        pid,
+                        e.desc(),
+                        e as i32
+                    );
+                    exit(1);
+                }
+                let _ = tcsetpgrp(stdin().as_fd(), Pid::from_raw(pid));
+                //* a new process group is created which has just the command and the pgid is same as pid of the child */
+                let full_path = CString::new(full_path.as_os_str().as_bytes()).unwrap_or_default();
+                if full_path.is_empty() {
+                    eprintln!("Pid: {}, Cannot get the executible path: {}", pid, command);
+                    exit(1);
+                }
+                let mut args_for_new_proc = Vec::with_capacity(args.len() + 1);
+                args_for_new_proc.push(CString::new(command.as_bytes()).unwrap_or_default());
+                for arg in args {
+                    args_for_new_proc.push(CString::new(arg.as_bytes()).unwrap_or_default());
+                }
+                match execv(&full_path, &args_for_new_proc) {
+                    Ok(_) => unreachable!(),
+                    Err(e) => {
+                        eprintln!(
+                            "Pid: {},Command: {}: execution failed: {}(Code: {})",
+                            pid,
+                            command,
+                            e.desc(),
+                            e as i32
+                        );
+                        exit(1)
+                    }
+                }
             }
             Ok(ForkResult::Parent { child }) => {
-                let w = waitpid(child, None);
+                let stdin = stdin();
+                let std_in_fd = stdin.as_fd();
+                let _ = setpgid(child, child);
+                let _ = tcsetpgrp(std_in_fd, child);
+                let _ = waitpid(child, None);
+                let _ = tcsetpgrp(std_in_fd, getpgrp());
             }
             Err(e) => {
                 eprintln!("Command execution failed: {}(Code: {})", e.desc(), e as i32);
@@ -77,6 +134,16 @@ fn handle_non_builtins(command: &str, args: &[&str]) {
 }
 
 fn main() {
+    /* ignoring some signals */
+    let sa = SigAction::new(
+        nix::sys::signal::SigHandler::SigIgn,
+        SaFlags::empty(),
+        SigSet::empty(),
+    );
+    unsafe {
+        let _ = sigaction(Signal::SIGTTOU, &sa);
+        let _ = sigaction(Signal::SIGTTIN, &sa);
+    }
     let stdin = std::io::stdin();
     let mut stdin_handle = stdin.lock();
     let mut stdin_buffer = String::new();
